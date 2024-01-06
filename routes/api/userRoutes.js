f2782a0ef7667f13d0e56373262f96a18d233b81
trnigg <tim@nigg.com.au>
2024-01-06 11:17:10 +0800

    Adds userRoutes with userController methods

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,22 @@
 const router = require('express').Router();
+// IMPORT controllers from the userController.js
+const {
+  getAllUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
