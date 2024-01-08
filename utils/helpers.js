7b482ba21bf0cccb504b683272f673cedc331751
trnigg <tim@nigg.com.au>
2024-01-08 13:18:02 +0800

    Adds improved date/time formatting for getters

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,10 @@
+// REQUIRED third-party module
+const { format } = require('date-fns');
+
+// https://date-fns.org/v3.1.0/docs/format
+// FOR GETTERS
 module.exports = {
-  // FOR GETTERS
   formatTimestamp: (timestamp) => {
-    return timestamp.toLocaleString();
+    return format(timestamp, "do 'of' MMM, yyyy 'at' hh:mm a");
   },
 };
